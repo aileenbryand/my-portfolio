956399d4336c6be6772c72423be195df87f094a4
Aileen Bryand <aileenale09@gmail.com>
2022-08-30 00:43:32 -0500

    color changes and Work added

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -3,21 +3,21 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (
-    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#D5BADB]'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-green-600'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        <p className='text-[#563C5C]'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#86608E]'>
           Aileen Bryand
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#563C5C]'>
           I'm a Full Stack Developer.
         </h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+        <p className='text-[#86608E] py-4 max-w-[700px]'>
         I am a 22 year old Full Stack Developer eager to continue learning and to continue to expand my knowledge in web development. I would like to present the projects that I worked on (along with several team memebrs) during my time in UT Austing Coding Bootcamp
         </p>
         <div>
-          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-green-600 hover:border-green-600'>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#301934] hover:border-[#301934]'>
             View Work
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     const [nav, setNav] = useState(false)
     const handleClick = () => setNav(!nav)
     return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0E2440] text-gray-300' >
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#301934] text-gray-300' >
 
            {/*menu*/} 
             
